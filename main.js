53141644984016019b4ea5ec554fa8cc3ba31260
peehu303 <84406912+peehu303@users.noreply.github.com>
2021-08-25 15:06:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+canvas = document.getElementById("myCanvas");
+ctx= canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.strokeStyle="grey";
+ctx.lineWidth=4;
+ctx.rect(150,143,430,200);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle="blue";
+ctx.lineWidth=4;
+ctx.arc(250,210 ,40,0,2*Math.PI);
+ctx.stroke();
+
+
+
+ctx.beginPath();
+ctx.strokeStyle="black";
+ctx.lineWidth=4;
+ctx.arc(370,210 ,40,0,2*Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle="red";
+ctx.lineWidth=4;
+ctx.arc(490,210 ,40,0,2*Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle="yellow";
+ctx.lineWidth=4;
+ctx.arc(310,240 ,40,0,2*Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle="green";
+ctx.lineWidth=4;
+ctx.arc(430,240 ,40,0,2*Math.PI);
+ctx.stroke();
+
